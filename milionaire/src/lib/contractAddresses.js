a6015c2c8732bad5f0ab82f6631814b1afc39284
vladi-coti <vladi@coti.io>
2026-06-23 11:29:07 +0300

    update(contractAddresses): contracts deployed

--- milionaire/src/lib/contractAddresses.js
+++ milionaire/src/lib/contractAddresses.js
@@ -5,9 +5,9 @@
  * an empty string until that network is deployed.
  */
 export const MILLIONAIRE_CONTRACT_ADDRESSES = {
-    7082400: '', // COTI Testnet
-    11155111: '', // Sepolia
-    43113: '', // Avalanche Fuji
+    7082400: '0xE8d8CAAeB1256e0A29Fe266Cc8037e1861354177', // COTI Testnet
+    11155111: '0x77e1Bc02FFa7A321a6492452170b173546e42511', // Sepolia
+    43113: '0xa39CBB61bc004823D761A381ccD69eE2d186F42A', // Avalanche Fuji
 };
 
 export function configuredAddress(address) {
